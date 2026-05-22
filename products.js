b8ecf009605714a70811ea93750aae719d8ddc7f
RubenCosta58 <rubencosta58@gmail.com>
2026-05-22 15:22:44 +0100

    Create products.js

---
+++ products.js
@@ -0,0 +1,87 @@
+export const products = [
+  { ref: "Piccola", sqmBox: 0.68, kgBox: 13.2 },
+
+  { ref: "Agadir", sqmBox: 0.68, kgBox: 13.2 },
+
+  { ref: "Alfar 6,5x20", sqmBox: 0.624, kgBox: 8.64 },
+
+  { ref: "Altea 7,5x30", sqmBox: 0.4949, kgBox: 7.92 },
+
+  { ref: "Antiga 6,9x24", sqmBox: 0.7451, kgBox: 10.8 },
+
+  { ref: "Atelier 7,5x15", sqmBox: 0.5399, kgBox: 9.12 },
+
+  { ref: "Atelier 7,5x30", sqmBox: 0.4949, kgBox: 7.92 },
+
+  { ref: "Atlantique 6,9x24", sqmBox: 0.7451, kgBox: 10.71 },
+
+  { ref: "Atlantique 20x20", sqmBox: 0.68, kgBox: 13.18 },
+
+  { ref: "Bali", sqmBox: 1, kgBox: 18 },
+
+  { ref: "Berlin", sqmBox: 1, kgBox: 18 },
+
+  { ref: "Black & White", sqmBox: 1, kgBox: 18 },
+
+  { ref: "Chicago", sqmBox: 1, kgBox: 18 },
+
+  { ref: "Crackle", sqmBox: 0.68, kgBox: 13 },
+
+  { ref: "Doria", sqmBox: 1, kgBox: 18 },
+
+  { ref: "Exa", sqmBox: 1, kgBox: 18 },
+
+  { ref: "Flat", sqmBox: 1.2599, kgBox: 20.16 },
+
+  { ref: "Flat Deluxe", sqmBox: 1.2599, kgBox: 20.16 },
+
+  { ref: "Flora Lapis", sqmBox: 1, kgBox: 18 },
+
+  { ref: "Fragmenta 20x20", sqmBox: 0.68, kgBox: 13.19 },
+
+  { ref: "Golden Stone", sqmBox: 1, kgBox: 18 },
+
+  { ref: "Granadella", sqmBox: 1, kgBox: 18 },
+
+  { ref: "Greenland", sqmBox: 1, kgBox: 18 },
+
+  { ref: "Iceberg", sqmBox: 1, kgBox: 18 },
+
+  { ref: "Kit-Kat", sqmBox: 0.68, kgBox: 13 },
+
+  { ref: "Meknes 6,2x25", sqmBox: 0.5579, kgBox: 8.1 },
+
+  { ref: "Metro 10x20", sqmBox: 0.8, kgBox: 10.4 },
+
+  { ref: "Metro 7,5x15", sqmBox: 1.0124, kgBox: 12.6 },
+
+  { ref: "Mijaran", sqmBox: 1, kgBox: 18 },
+
+  { ref: "Milano 6,9x24", sqmBox: 0.7451, kgBox: 10.62 },
+
+  { ref: "Milano 20x20", sqmBox: 0.68, kgBox: 13.11 },
+
+  { ref: "Mintons", sqmBox: 1, kgBox: 18 },
+
+  { ref: "Renania 6,5x20", sqmBox: 0.624, kgBox: 8.64 },
+
+  { ref: "Riad", sqmBox: 1, kgBox: 18 },
+
+  { ref: "Saudade", sqmBox: 1, kgBox: 18 },
+
+  { ref: "Sea", sqmBox: 1, kgBox: 18 },
+
+  { ref: "Tabarca 7,5x23", sqmBox: 0.4829, kgBox: 7 },
+
+  { ref: "Tabarca 15x15", sqmBox: 0.675, kgBox: 10.26 },
+
+  { ref: "Tahiti", sqmBox: 1, kgBox: 18 },
+
+  { ref: "Terracota", sqmBox: 1, kgBox: 18 },
+
+  { ref: "Terraluz", sqmBox: 1, kgBox: 18 },
+
+  { ref: "Valencia", sqmBox: 1, kgBox: 18 },
+
+  { ref: "Xabia", sqmBox: 1, kgBox: 18 },
+];
