1fe4d31dde300ad71206f3e1af1892a9746c6ed6
RubenCosta58 <rubencosta58@gmail.com>
2026-05-25 15:38:10 +0100

    Update products.js

--- products.js
+++ products.js
@@ -1,35 +1,315 @@
 export const products = [
 
+  // VALENCIA
+
   {
-    id: "188576",
-    collection: "Agadir",
-    color: "Agua",
-    format: "14,7x14,7",
-    code: "M926",
+    ref: "188470N",
+    name: "Valencia Blanco",
+    collection: "Valencia",
+    color: "Blanco",
+    code: "M921",
+    size: "20x20",
+    boxM2: 1.36,
+    boxKg: 14.03,
+    pvp: 24.90
   },
 
   {
-    id: "188577",
-    collection: "Agadir",
-    color: "Lago",
-    format: "14,7x14,7",
-    code: "M926",
+    ref: "188471N",
+    name: "Valencia Coral",
+    collection: "Valencia",
+    color: "Coral",
+    code: "M921",
+    size: "20x20",
+    boxM2: 1.36,
+    boxKg: 14.03,
+    pvp: 24.90
   },
 
   {
-    id: "188251",
+    ref: "188472N",
+    name: "Valencia Salvia",
+    collection: "Valencia",
+    color: "Salvia",
+    code: "M921",
+    size: "20x20",
+    boxM2: 1.36,
+    boxKg: 14.03,
+    pvp: 24.90
+  },
+
+  {
+    ref: "188473N",
+    name: "Valencia Azul",
+    collection: "Valencia",
+    color: "Azul",
+    code: "M921",
+    size: "20x20",
+    boxM2: 1.36,
+    boxKg: 14.03,
+    pvp: 24.90
+  },
+
+  {
+    ref: "188474N",
+    name: "Valencia Palmar",
+    collection: "Valencia",
+    color: "Palmar",
+    code: "M923",
+    size: "20x20",
+    boxM2: 1.36,
+    boxKg: 14.03,
+    pvp: 32.90
+  },
+
+  {
+    ref: "188475N",
+    name: "Valencia Carmen",
+    collection: "Valencia",
+    color: "Carmen",
+    code: "M923",
+    size: "20x20",
+    boxM2: 1.36,
+    boxKg: 14.03,
+    pvp: 32.90
+  },
+
+  {
+    ref: "188476N",
+    name: "Valencia Jordana",
+    collection: "Valencia",
+    color: "Jordana",
+    code: "M923",
+    size: "20x20",
+    boxM2: 1.36,
+    boxKg: 14.03,
+    pvp: 32.90
+  },
+
+  {
+    ref: "188477N",
+    name: "Valencia Remor",
+    collection: "Valencia",
+    color: "Remor",
+    code: "M923",
+    size: "20x20",
+    boxM2: 1.36,
+    boxKg: 14.03,
+    pvp: 32.90
+  },
+
+  {
+    ref: "188478N",
+    name: "Valencia Sorolla",
+    collection: "Valencia",
+    color: "Sorolla",
+    code: "M923",
+    size: "20x20",
+    boxM2: 1.36,
+    boxKg: 14.03,
+    pvp: 32.90
+  },
+
+  {
+    ref: "188479N",
+    name: "Valencia Turia",
+    collection: "Valencia",
+    color: "Turia",
+    code: "M923",
+    size: "20x20",
+    boxM2: 1.36,
+    boxKg: 14.03,
+    pvp: 32.90
+  },
+
+  // TABARCA
+
+  {
+    ref: "188251N",
+    name: "Tabarca Verde",
     collection: "Tabarca",
     color: "Verde",
-    format: "15x15",
     code: "M894",
+    size: "15x15",
+    boxM2: 0.675,
+    boxKg: 10.26,
+    pvp: 52.90
   },
 
   {
-    id: "189192",
+    ref: "188250N",
+    name: "Tabarca Cielo",
+    collection: "Tabarca",
+    color: "Cielo",
+    code: "M894",
+    size: "15x15",
+    boxM2: 0.675,
+    boxKg: 10.26,
+    pvp: 52.90
+  },
+
+  {
+    ref: "188249N",
+    name: "Tabarca Marino",
+    collection: "Tabarca",
+    color: "Marino",
+    code: "M894",
+    size: "15x15",
+    boxM2: 0.675,
+    boxKg: 10.26,
+    pvp: 52.90
+  },
+
+  // AGADIR
+
+  {
+    ref: "188576",
+    name: "Agadir Agua",
+    collection: "Agadir",
+    color: "Agua",
+    code: "M926",
+    size: "14,7x14,7",
+    boxM2: 0.9507,
+    boxKg: 15.4,
+    pvp: 64.90
+  },
+
+  {
+    ref: "188577",
+    name: "Agadir Lago",
+    collection: "Agadir",
+    color: "Lago",
+    code: "M926",
+    size: "14,7x14,7",
+    boxM2: 0.9507,
+    boxKg: 15.4,
+    pvp: 64.90
+  },
+
+  {
+    ref: "188578",
+    name: "Agadir Cotto",
+    collection: "Agadir",
+    color: "Cotto",
+    code: "M926",
+    size: "14,7x14,7",
+    boxM2: 0.9507,
+    boxKg: 15.4,
+    pvp: 64.90
+  },
+
+  // MILANO
+
+  {
+    ref: "189192",
+    name: "Milano Bosco",
     collection: "Milano",
     color: "Bosco",
-    format: "20x20",
     code: "M921",
+    size: "20x20",
+    boxM2: 1,
+    boxKg: 18.2,
+    pvp: 61.50
   },
+
+  {
+    ref: "189193",
+    name: "Milano Cenere",
+    collection: "Milano",
+    color: "Cenere",
+    code: "M921",
+    size: "20x20",
+    boxM2: 1,
+    boxKg: 18.2,
+    pvp: 61.50
+  },
+
+  {
+    ref: "189194",
+    name: "Milano Avorio",
+    collection: "Milano",
+    color: "Avorio",
+    code: "M921",
+    size: "20x20",
+    boxM2: 1,
+    boxKg: 18.2,
+    pvp: 61.50
+  },
+
+  // BERLIN
+
+  {
+    ref: "188620",
+    name: "Berlin White",
+    collection: "Berlin",
+    color: "White",
+    code: "M940",
+    size: "7x28",
+    boxM2: 0.59,
+    boxKg: 11.2,
+    pvp: 79.90
+  },
+
+  {
+    ref: "188621",
+    name: "Berlin Sage",
+    collection: "Berlin",
+    color: "Sage",
+    code: "M940",
+    size: "7x28",
+    boxM2: 0.59,
+    boxKg: 11.2,
+    pvp: 79.90
+  },
+
+  {
+    ref: "188622",
+    name: "Berlin Navy",
+    collection: "Berlin",
+    color: "Navy",
+    code: "M940",
+    size: "7x28",
+    boxM2: 0.59,
+    boxKg: 11.2,
+    pvp: 79.90
+  },
+
+  // PIETRASANTA
+
+  {
+    ref: "189330",
+    name: "Pietrasanta Grey",
+    collection: "Pietrasanta",
+    color: "Grey",
+    code: "M967",
+    size: "7x28",
+    boxM2: 0.59,
+    boxKg: 11.5,
+    pvp: 89.90
+  },
+
+  {
+    ref: "189331",
+    name: "Pietrasanta Ivory",
+    collection: "Pietrasanta",
+    color: "Ivory",
+    code: "M967",
+    size: "7x28",
+    boxM2: 0.59,
+    boxKg: 11.5,
+    pvp: 89.90
+  },
+
+  {
+    ref: "189332",
+    name: "Pietrasanta Dark",
+    collection: "Pietrasanta",
+    color: "Dark",
+    code: "M967",
+    size: "7x28",
+    boxM2: 0.59,
+    boxKg: 11.5,
+    pvp: 89.90
+  }
 
 ];
